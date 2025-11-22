07d261240e5ffb1fe6e0e38b4cbf00560be44aa8
syeony <fltkfltk1004@naver.com>
2025-11-23 00:54:09 +0900

    feat: Clothes API DTO 정의

---
+++ weatherfit/app/src/main/java/com/woowa/weatherfit/data/remote/dto/ClothesRequest.kt
@@ -0,0 +1,13 @@
+package com.woowa.weatherfit.data.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ClothesRegisterRequest(
+    @SerialName("category")
+    val category: String,
+
+    @SerialName("subCategory")
+    val subCategory: String
+)
